97e7f40e24d8480c0619713351705f215134e2d7
ReinaDNA <xxzhijiexx@gmail.com>
2025-12-30 20:58:23 +0800

    construct PCB
    Start the project by constructing PCB class

---
+++ main.py
@@ -0,0 +1,6 @@
+from pcb import Pcb
+def main():
+    pass
+
+
+main()
---
+++ pcb.py
@@ -0,0 +1,15 @@
+from random import randint
+
+class Pcb:
+    def __init__(self, number):
+        self.program_number = number
+        self.arrival_time = randint(0,100) #Program arrival times are randomly generated between 0 and 100ms.
+        self.burst_time = randint(5,50) #Program burst times are randomly generated between 5 and 50ms.
+        self.start_time = 0 #Time when the program starts getting processed.
+        self.finish_time = 0
+        self.turnaround_time = 0
+        self.waiting_time = 0
+        
+        
+    def getArrivalTime(self):
+        return self.arrival_time
