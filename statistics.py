241d845cc10ded6c9e555e073b756ceec4b6d8f7
ReinaDNA <xxzhijiexx@gmail.com>
2025-12-31 00:19:25 +0800

    statistics
    new class for all the metric calculations required for report

---
+++ statistics.py
@@ -0,0 +1,11 @@
+from pcb import Pcb
+
+class Statistics:
+    def __init__(self):
+        self.completed_processes: list[Pcb] = []
+
+    def calculateAverageWaitingTime(self):
+        pass
+
+    def calculateAverageTurnaroundTime(self):
+        pass
