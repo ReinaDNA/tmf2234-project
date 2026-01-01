758bcd0884a375bc27c45578618dcc809a09bc38
ReinaDNA <xxzhijiexx@gmail.com>
2026-01-02 00:28:36 +0800

    finish one cycle
    CPU can now finish one cycle, fixed some bugs in filtering available jobs and did some refactoring

--- queues.py
+++ queues.py
@@ -2,17 +2,24 @@ from pcb import Pcb
 
 class ReadyQueue:
     def __init__(self):
-        self.queuing_list = []
+        self.queuing_list:list[Pcb] = []
         
     def addProcess(self, newProcess:Pcb):
         self.queuing_list.append(newProcess)
-
-    def fetchNextProcess(self):
+   
+    def removeFirstProcess(self) -> Pcb | None:
         if self.queuing_list:
+            self.queuing_list[0].changeState(Pcb.RUNNING)
             return self.queuing_list.pop(0)
         else:
             print("No process found.")
-            
+
     def getQueueList(self):
         return self.queuing_list
     
+class CompletedQueue:
+    def __init__(self):
+        self.completed_list: list[Pcb] = []
+
+    def addProcess(self, newProcess:Pcb):
+        self.completed_list.append(newProcess)
--- scheduler.py
+++ scheduler.py
@@ -5,12 +5,33 @@ class Scheduler:
         self.ready_queue = ReadyQueue()
         self.available_jobs: list[Pcb] = available_jobs
 
-    def processChecker(self, time):
+    def filterAvailableProcess(self, time):
+        not_ready_jobs: list[Pcb] = []
         if self.available_jobs:
-            for i, job in enumerate(self.available_jobs):
+            for job in self.available_jobs:
                 if job.getArrivalTime() <= time:
                     print(f"Job {job.getProgramNumber()} arrived at {job.getArrivalTime()}")
-                    ready_job = self.available_jobs.pop(i)
-                    self.ready_queue.addProcess(ready_job)
+                    self.ready_queue.addProcess(job)
                     print(f"Job {job.getProgramNumber()} added to ready queue")
-        return self.ready_queue
+                else:
+                    not_ready_jobs.append(job)
+            self.available_jobs = not_ready_jobs
+            return self.ready_queue.getQueueList()
+        else:
+            print("No available jobs to schedule.")
+    
+    def fetchNextProcess(self) -> Pcb | None:
+        return self.ready_queue.removeFirstProcess()
+
+    def checkAvailableProcesses(self):
+        if self.available_jobs:
+            return True
+        else:
+            return False        
+
+    def checkExecutedProcess(self, process:Pcb):
+        if process.getCurrentState() == Pcb.COMPLETED:
+            return process
+        else:
+            self.ready_queue.addProcess(process)
+            return None
